2c016e8a58d8d7c25a8eea00bf6fd147d3cfa18c
Aman Jain <amanj120@gmail.com>
2019-02-14 07:19:30 -0500

    Update val.py

--- val/val.py
+++ val/val.py
@@ -1,39 +1,32 @@
 import math
 
-def PrimeSieve(n):
-    lastPrime = 5;
+def PrimeSieve(n, lastPrime):
     while(len(primes) < n):
         if(isPrime(lastPrime)):
             primes.append(lastPrime)
         lastPrime += 2
         
 def isPrime(n):
-    for div in range (2,math.ceil(math.sqrt(n)) + 2):
+    for div in range (3,math.ceil(math.sqrt(n)) + 2):
         if(n % div == 0):
             return False
+        div += 1
     return True
 
 #inputFile = open("input.txt" , "r")
 line = "Hi  dweb pee lol"
 chars = []
-code = []
 primes = [1,2,3]
 #for line in inputFile:
 for word in line.split():
-    word = word.lower()
     for c in range (len(word)):
-        chars.append(ord(word[c]) - 97)
-        #print(word[c])
-        #print(chars[-1])
-PrimeSieve(len(chars))
-print(chars)
-print(primes)
+        chars.append(ord(word[c].lower()) - 97)
+PrimeSieve(len(chars),5)
+print(line)
 i = 0
 for char in line:
     if char == " " or char == "." or char == ",":
-        code.append(char)
+        print(char, end="")
     else :
-        code.append(chr(((chars[i] + primes[i])%26)+97))
+        print((chr(((chars[i] + primes[i])%26)+97)), end="")
         i += 1
-print(line)
-print("".join(code))
